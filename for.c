e6b058f6c5d2851a2f0ae8e71df0581ce6086b94
chaitrajoshi28 <92718534+chaitrajoshi28@users.noreply.github.com>
2024-11-18 16:30:19 +0530

    Add files via upload

---
+++ for.c
@@ -0,0 +1,13 @@
+//Program to print numbers from 1 to 10 using a for loop.
+
+#include <stdio.h>
+
+int main() {
+    // Using a for loop to print numbers from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i); // Print the current value of i
+    }
+
+    return 0; // Indicate that the program ended successfully
+}
+
