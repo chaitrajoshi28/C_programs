227475d149e40c6ebe37734aefd29ba5eaf864fa
chaitrajoshi28 <92718534+chaitrajoshi28@users.noreply.github.com>
2024-11-20 11:54:33 +0530

    Add files via upload

--- Helloworld.c
+++ Helloworld.c
@@ -3,7 +3,12 @@
 
 # include<stdio.h>
 int main() {
+    // psedocode:start the program
+    //psedocode:Display "Hello World!" message 
+    //print "Hellow World!" 
+    
     printf("Hello World!\n");
+    //psedocode: End the program
     return 0; //indicates program has executed successfully
 }
 
