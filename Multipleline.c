6ad5d46bd0cdf9d39d02c6fc87f69cb8c0938113
chaitrajoshi28 <92718534+chaitrajoshi28@users.noreply.github.com>
2024-11-20 12:00:33 +0530

    Add files via upload

--- Multipleline.c
+++ Multipleline.c
@@ -1,11 +1,12 @@
-//Program to print multiple lines of text using a single output statement.
-//Example: Printing student address
 # include<stdio.h>
+
 int main() {
+    // psedocode: Start the program
     // This statement prints multiple line of text using single printf function 
+    // print the Name,Address ,City, state.
     printf("Name: Jhon \n"
            "Address: #24 Main street\n"
            "City :Bengaluru\n"
             "State:Karnataka\n" );
-            return 0;
+            return 0; // End of program
 }
